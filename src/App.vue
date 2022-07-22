c676d232e966affe97dbc82e5e85a6cf89950883
Etienne de Martel <edemartel@gmail.com>
2022-07-21 22:12:56 -0400

    Vue.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import Map from "./components/Map.vue";
+
+export default {
+  components: {
+    Map,
+  }
+};
+</script>
+
+<template>
+  <Map></Map>
+</template>
---
+++ src/components/Map.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script lang="ts">
+import "leaflet/dist/leaflet.css"
+import L from "leaflet";
+
+export default {
+    mounted() {
+        var map = new L.Map("map", {
+            center: [45.5001, -73.5679],
+            zoom: 15
+        });
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            maxZoom: 19,
+            attribution: "&copy; OpenStreetMap"
+        }).addTo(map);
+    }
+}
+</script>
+
+<style scoped>
+#map {
+    width: 1000px;
+    height: 800px;
+}
+</style>
