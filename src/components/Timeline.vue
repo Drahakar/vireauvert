7bda3b37ae990c6582c51091555015d560bf9836
Andy Emond <emondmeister@gmail.com>
2022-07-30 15:32:11 -0400

    ajout de l'axe du temps

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,14 +1,35 @@
 <template>
     <div id="timeline">
-2022
+        <div class="slidercontainer">
+            <vue-slider 
+                v-model="selectedYear" 
+                :tooltip="'active'" 
+                :min="1970" 
+                :max="2050" 
+                :interval="10" 
+                :marks="true"
+                :adsorb="false"></vue-slider>
+        </div> 
     </div>
 </template>
 
 <script lang="ts">
-export default {
+import VueSlider from 'vue-slider-component'
+import { defineComponent } from 'vue';
+import 'vue-slider-component/theme/default.css'
+
+export default defineComponent({
     async mounted() {
+    },
+    data: function() {
+        return {
+            selectedYear: 2022
+        }
+    },
+    components: {
+        VueSlider
     }
-}
+})
 </script>
 
 <style scoped>
@@ -16,4 +37,22 @@ export default {
     width: 100%;
     height: 10%;
 }
+.slidercontainer {
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    z-index: 29;
+    padding: 0px 30px;
+}
+
+.slidercontainer input {
+    -webkit-appearance: none;
+    width: 100%;
+    height: 100%;
+    background: #ffffff;
+    outline: none;
+    opacity: 1
+}
+
 </style>
