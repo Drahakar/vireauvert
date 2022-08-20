84191ee5fcf6ec1a6f477bbaed2638a5b5d203c0
Andy Emond <emondmeister@gmail.com>
2022-08-19 21:42:55 -0400

    ajout du nombre d'évènement au dessus de la ligne du temps

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -3,16 +3,26 @@
         <div id="slidertitle" class="col-md-2">{{ $t('research_year') }}</div>
         <div id="slidercontainer" class="col-md-10">
             <vue-slider v-model="selectedYear" :tooltip="'always'" :data="years" :marks="marks" :adsorb="false">
+                <template v-slot:label="{value}">
+                    <div :class="['vue-slider-mark-label', 'custom-label']">{{value}}</div>
+                    <div :class="['vue-slider-mark-label', 'custom-label', 'event-count', 'badge', 'text-bg-danger']">{{catastrophesCountByYears(value)}}</div>
+                </template>
+                  <template v-slot:step="{value}">
+                    <div :class="['vue-slider-mark-label', 'custom-label', 'event-count']">{{catastrophesCountByYears(value)}}</div>
+                </template>
             </vue-slider>
+            
         </div>
     </div>
 </template>
 
 <script lang="ts">
 import VueSlider from 'vue-slider-component'
-import { computed, defineComponent } from 'vue';
+import { computed, PropType, defineComponent } from 'vue';
 import 'vue-slider-component/theme/default.css'
 import { TIMELINE_YEARS, } from '@/models/constants';
+import { useCatastropheStore } from '@/stores/catastrophes';
+import { CatastropheFilter } from '@/models/catastrophes';
 
 export default defineComponent({
     components: { VueSlider },
@@ -25,6 +35,14 @@ export default defineComponent({
         year: {
             type: Number,
             default: 0
+        },
+        catastropheFilter: {
+            type: String as PropType<CatastropheFilter>,
+            default: ''
+        },
+        district: {
+            type: Number,
+            default: 0
         }
     },
     setup(props, { emit }) {
@@ -32,15 +50,24 @@ export default defineComponent({
             get: () => props.year,
             set: value => emit('yearSelected', value)
         });
-        return { selectedYear };
+        const catastropheStore = useCatastropheStore();
+        return { 
+            selectedYear,
+            catastropheStore, 
+        };
     },
     data() {
         return {
             marks: TIMELINE_YEARS.filter(x => x % (this.isMobile ? 10 : 5) === 0),
             years: TIMELINE_YEARS
         }
+    },
+    methods: {
+        catastrophesCountByYears(year: number): Number {
+            return this.catastropheStore.findCatastrophes(year, this.district, this.catastropheFilter).size;
+        }
     }
-})
+});
 </script>
 
 <style scoped>
@@ -58,5 +85,9 @@ export default defineComponent({
     background: #ffffff;
     outline: none;
     opacity: 1
+}
+
+.vue-slider-mark-label.custom-label.event-count {
+    top:-32px;
 }
 </style>
