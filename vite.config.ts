7387d9a05bcd5b7e0b16f21309940422784a8ece
Etienne de Martel <edemartel@gmail.com>
2022-08-21 21:31:33 -0400

    Config nécessaire pour rouler de l'interpolation de loc en prod.

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,15 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueI18n({
-      compositionOnly: true,
-      include: ['./src/locales/**']
+      runtimeOnly: false,
+      include: path.resolve(__dirname, "./src/locales/**"),
     })
   ],
   resolve: {
