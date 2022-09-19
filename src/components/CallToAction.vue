1b3817942a8b3dbb02d79d709e0bf91d70d0d1ff
JesseEmond <emond.jesse@gmail.com>
2022-09-18 23:00:40 -0400

    Replace call-to-action with image button

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -1,6 +1,7 @@
 <template>
     <a href="https://www.vireauvert.org/envoipropositionspartispolitiques"
-        role="button" target="_blank" v-t="'call_to_action'">
+        role="button" target="_blank" :aria-label="$t('call_to_action')">
+        <img src="/call_to_action.png">
     </a>
 </template>
 
@@ -13,15 +14,19 @@ export default defineComponent({
 </script>
 <style scoped>
 a {
-    font-size: var(--sz-400);
-    color: var(--clr-blanc);
-    background-color: var(--color-accent);
-    border-radius: var(--border-radius);
-    width: min-content;
-    border: none;
     cursor: pointer;
-    padding: var(--sz-400) var(--sz-30);
     text-align: center;
     text-decoration: none;
+    border-radius: 50%;
+    width: calc(var(--sz-900) * 2);
+    height: calc(var(--sz-900) * 2);
+}
+
+img {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25),
+        inset 0px 0px 2px rgba(0, 0, 0, 0.5);
 }
 </style>
